9a54af5ca58cc330363c442b35311370560f69a6
zohoMatt <mattzuo12@gmail.com>
2020-04-22 17:04:02 -0400

    feat: Table demo with React-router.

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,13 +1,31 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
-
-import CounterContainer from '../containers/CounterContainer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Table from './common/Table/Table';
 
 const Application = () => (
-    <div>
-        Hello World from Electron!
-        <CounterContainer />
-    </div>
+    <Router>
+        <div>
+            <Switch>
+                <Route path="/demo">
+                    <Table
+                        columns={[
+                            { key: 'n', text: 'Zone' },
+                            { key: 'component', text: 'Components' },
+                            { key: 'bvf', text: 'Bed Volume Fed' }
+                        ]}
+                        data={[
+                            { n: 1, component: 'trichhloroethylene', bvf: 0.00152 },
+                            { n: 2, component: 'chloroform', bvf: 0.00532 }
+                        ]}
+                        />
+                </Route>
+                <Route path="/">
+                    <h1>Welcome page</h1>
+                </Route>
+            </Switch>
+        </div>
+    </Router>
 );
 
 export default hot(Application);
