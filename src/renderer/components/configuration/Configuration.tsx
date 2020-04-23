6f79da9112387f9784d9d596f47a3081b5ca45b2
zohoMatt <mattzuo12@gmail.com>
2020-04-23 19:59:08 -0400

    feat: Nested router.

--- src/renderer/components/configuration/Configuration.tsx
+++ src/renderer/components/configuration/Configuration.tsx
@@ -1,1 +1,14 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, useRouteMatch } from 'react-router-dom';
+
+import NavBar from '../nav/NavBar';
+
+const ConfigurationPage: React.FunctionComponent = () => {
+    const match = useRouteMatch();
+    return (
+        <div>
+            <NavBar />
+            <Switch />
+        </div>
+    );
+};
--- src/renderer/components/nav/NavBar.tsx
+++ src/renderer/components/nav/NavBar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import { Menu } from 'antd';
 import { AreaChartOutlined, PaperClipOutlined, InsertRowBelowOutlined } from '@ant-design/icons';
@@ -9,59 +10,86 @@ const { SubMenu, ItemGroup, Item } = Menu;
 // Hack for tslint
 const Group = ItemGroup as any;
 
-const NavBar: React.FunctionComponent = () => (
-    <div className={styles.navbar}>
-        <Menu style={{ width: '100%' }} mode="inline">
-            <SubMenu
-                key="sub1"
-                title={
-                    <span>
-                        <PaperClipOutlined />
-                        <span>Settings</span>
-                    </span>
-                }>
-                <Group key="water" title="Water">
-                    <Item key="water.props">Properties</Item>
-                </Group>
-                <Group key="component" title="Components">
-                    <Item key="component.props">Properties</Item>
-                    <Item key="component.model">Adsorption Model</Item>
-                </Group>
-                <Group key="sim" title="Simulation Parameters">
-                    <Item key="sim.psdm">PSDM</Item>
-                </Group>
-                <Group key="fixedBed" title="Fixed Bed">
-                    <Item key="fixedBed.adsorber">Adsorber</Item>
-                    <Item key="fixedBed.props">Bed Properties</Item>
-                </Group>
-                <Group key="adsorbent" title="Adsorbent">
-                    <Item key="adsorbent.database">Database</Item>
-                    <Item key="adsorbent.props">Properties</Item>
-                </Group>
-            </SubMenu>
-            <SubMenu
-                key="sub2"
-                title={
-                    <span>
-                        <InsertRowBelowOutlined />
-                        <span>Data Sets</span>
-                    </span>
-                }>
-                <Item key="comb">Combinations</Item>
-            </SubMenu>
-            <SubMenu
-                key="sub3"
-                title={
-                    <span>
-                        <AreaChartOutlined />
-                        <span>Plot</span>
-                    </span>
-                }>
-                <Item key="results">Results</Item>
-                <Item key="comparison">Comparison</Item>
-            </SubMenu>
-        </Menu>
-    </div>
-);
+const NavBar: React.FunctionComponent = () => {
+    const match = useRouteMatch();
+    return (
+        <div className={styles.navbar}>
+            <Menu style={{ width: '100%' }} mode="inline">
+                <SubMenu
+                    key="sub1"
+                    title={
+                        <span>
+                            <PaperClipOutlined />
+                            <span>Settings</span>
+                        </span>
+                    }>
+                    <Group key="water" title="Water">
+                        <Item key="water.props">
+                            <Link to={`${match.url}/settings/water/prop`}>Properties</Link>
+                        </Item>
+                    </Group>
+                    <Group key="component" title="Components">
+                        <Item key="component.props">
+                            <Link to={`${match.url}/settings/components/prop`}>Properties</Link>
+                        </Item>
+                        <Item key="component.model">
+                            <Link to={`${match.url}/settings/components/model`}>
+                                Adsorption Model
+                            </Link>
+                        </Item>
+                    </Group>
+                    <Group key="sim" title="Simulation Parameters">
+                        <Item key="sim.psdm">
+                            <Link to={`${match.url}/settings/sim/psdm`}>PSDM</Link>
+                        </Item>
+                    </Group>
+                    <Group key="fixedBed" title="Fixed Bed">
+                        <Item key="fixedBed.adsorber">
+                            <Link to={`${match.url}/settings/bed/adsorber`}>Adsorber</Link>
+                        </Item>
+                        <Item key="fixedBed.props">
+                            <Link to={`${match.url}/settings/bed/prop`}>Bed Properties</Link>
+                        </Item>
+                    </Group>
+                    <Group key="adsorbent" title="Adsorbent">
+                        <Item key="adsorbent.database">
+                            <Link to={`${match.url}/settings/adsorbent/db`}>Database</Link>
+                        </Item>
+                        <Item key="adsorbent.props">
+                            <Link to={`${match.url}/settings/adsorbent/prop`}>Properties</Link>
+                        </Item>
+                    </Group>
+                </SubMenu>
+                <SubMenu
+                    key="sub2"
+                    title={
+                        <span>
+                            <InsertRowBelowOutlined />
+                            <span>Data Sets</span>
+                        </span>
+                    }>
+                    <Item key="comb">
+                        <Link to={`${match.url}/dataset/comb`}>Combinations</Link>
+                    </Item>
+                </SubMenu>
+                <SubMenu
+                    key="sub3"
+                    title={
+                        <span>
+                            <AreaChartOutlined />
+                            <span>Plot</span>
+                        </span>
+                    }>
+                    <Item key="results">
+                        <Link to={`${match.url}/plot/res`}>Results</Link>
+                    </Item>
+                    <Item key="comparison">
+                        <Link to={`${match.url}/plot/compare`}>Comparison</Link>
+                    </Item>
+                </SubMenu>
+            </Menu>
+        </div>
+    );
+};
 
 export default NavBar;
