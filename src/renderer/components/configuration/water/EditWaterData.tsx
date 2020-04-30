07d14ecc3e69c14eeb874794f38dca82309d8de9
zohoMatt <mattzuo12@gmail.com>
2020-04-29 22:53:55 -0400

    refact: Make EditWaterData component stateless.

--- src/renderer/components/configuration/water/EditWaterData.tsx
+++ src/renderer/components/configuration/water/EditWaterData.tsx
@@ -14,92 +14,93 @@ export interface EditWaterProps {
     onValuesChange: (params: ActiveEditing) => any;
 }
 
-export class EditWaterData extends React.Component<EditWaterProps> {
-    public render() {
-        const { form, initValues, onValuesChange } = this.props;
-        const { temperature } = initValues;
-        const NORMAL_RULES = [{ required: true, message: 'Value cannot be empty' }];
+export const EditWaterData: React.FunctionComponent<EditWaterProps> = ({
+    form,
+    initValues,
+    onValuesChange
+}) => {
+    const { temperature } = initValues;
+    const NORMAL_RULES = [{ required: true, message: 'Value cannot be empty' }];
 
-        return (
-            <div className={styles.container}>
-                <div className={styles.formContainer}>
-                    <Form
-                        size="small"
-                        layout="horizontal"
-                        ref={form}
-                        hideRequiredMark={true}
-                        labelCol={{ span: 6 }}
-                        wrapperCol={{ span: 8 }}
-                        onValuesChange={(s, all: any) => onValuesChange(all)}
-                        initialValues={initValues}>
-                        <Form.Item
-                            name="name"
-                            label="Name"
-                            rules={[...NORMAL_RULES, { max: 20, type: 'string' }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item
-                            name="description"
-                            label="Description"
-                            rules={[...NORMAL_RULES, { max: 80, type: 'string' }]}>
-                            <Input.TextArea rows={4} />
-                        </Form.Item>
-                        <Divider orientation="left">Basic</Divider>
-                        <Form.Item
-                            name="pressure"
-                            label="Pressure"
-                            rules={[
-                                ...NORMAL_RULES,
-                                {
-                                    min: 0.9,
-                                    max: 1.1,
-                                    type: 'number',
-                                    message: 'Warning: Abnormal value.',
-                                    validateTrigger: 'onChange'
-                                }
-                            ]}
-                            normalize={v => (v ? +v : '')}>
-                            <Input type="number" addonAfter="atm" />
-                        </Form.Item>
-                        <Form.Item
-                            name="temperature"
-                            label="Temperature"
-                            rules={[
-                                ...NORMAL_RULES,
-                                {
-                                    min: 0,
-                                    max: 100,
-                                    type: 'number',
-                                    validateTrigger: 'onChange'
-                                }
-                            ]}
-                            normalize={v => (v ? +v : '')}>
-                            <Input type="number" addonAfter="℃" />
-                        </Form.Item>
-                        <Divider orientation="left">Correlations</Divider>
-                        <Form.Item label="Density">
-                            <Form.Item name={['density', 'use']}>
-                                <TextSwitcher
-                                    text={
-                                        temperature
-                                            ? Calculation.display(Water.density(temperature))
-                                            : '-'
-                                    }
-                                    />
-                            </Form.Item>
-                        </Form.Item>
-                        <Form.Item name={['viscosity', 'use']} label="Viscosity">
+    return (
+        <div className={styles.container}>
+            <div className={styles.formContainer}>
+                <Form
+                    size="small"
+                    layout="horizontal"
+                    ref={form}
+                    hideRequiredMark={true}
+                    labelCol={{ span: 6 }}
+                    wrapperCol={{ span: 8 }}
+                    onValuesChange={(s, all: any) => onValuesChange(all)}
+                    initialValues={initValues}>
+                    <Form.Item
+                        name="name"
+                        label="Name"
+                        rules={[...NORMAL_RULES, { max: 20, type: 'string' }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name="description"
+                        label="Description"
+                        rules={[...NORMAL_RULES, { max: 80, type: 'string' }]}>
+                        <Input.TextArea rows={4} />
+                    </Form.Item>
+                    <Divider orientation="left">Basic</Divider>
+                    <Form.Item
+                        name="pressure"
+                        label="Pressure"
+                        rules={[
+                            ...NORMAL_RULES,
+                            {
+                                min: 0.9,
+                                max: 1.1,
+                                type: 'number',
+                                message: 'Warning: Abnormal value.',
+                                validateTrigger: 'onChange'
+                            }
+                        ]}
+                        normalize={v => (v ? +v : '')}>
+                        <Input type="number" addonAfter="atm" />
+                    </Form.Item>
+                    <Form.Item
+                        name="temperature"
+                        label="Temperature"
+                        rules={[
+                            ...NORMAL_RULES,
+                            {
+                                min: 0,
+                                max: 100,
+                                type: 'number',
+                                validateTrigger: 'onChange'
+                            }
+                        ]}
+                        normalize={v => (v ? +v : '')}>
+                        <Input type="number" addonAfter="℃" />
+                    </Form.Item>
+                    <Divider orientation="left">Correlations</Divider>
+                    <Form.Item label="Density">
+                        <Form.Item name={['density', 'use']}>
                             <TextSwitcher
                                 text={
                                     temperature
-                                        ? Calculation.display(Water.viscosity(temperature))
+                                        ? Calculation.display(Water.density(temperature))
                                         : '-'
                                 }
                                 />
                         </Form.Item>
-                    </Form>
-                </div>
+                    </Form.Item>
+                    <Form.Item name={['viscosity', 'use']} label="Viscosity">
+                        <TextSwitcher
+                            text={
+                                temperature
+                                    ? Calculation.display(Water.viscosity(temperature))
+                                    : '-'
+                            }
+                            />
+                    </Form.Item>
+                </Form>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
