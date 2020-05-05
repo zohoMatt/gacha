68a56a1f20c98889580f0ea4ada7ddc4e3cb8362
zohoMatt <mattzuo12@gmail.com>
2020-05-05 17:25:49 -0400

    feat: Contaminant store.

--- src/renderer/components/configuration/pollutant/AdsorptionModel.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-const AdsorptionModel: React.FunctionComponent = () => <h1>AdsorptionModel</h1>;
-
-export default AdsorptionModel;
---
+++ src/renderer/store/contaminant.store.ts
@@ -0,0 +1,84 @@
+import { v4 } from 'uuid';
+import { BasicTableWithEditStore } from './types';
+
+export interface ContaminantParams {
+    material: {
+        name: string;
+        description: string;
+        molecularWeight: number; // mg/mmol
+        molarVolume: number; // mL/gmol
+        boilingPt: number; // ℃
+        initConcent: number; // mg/L
+        liquidDensity: number; // g/mL
+        solubility: number; // mg/L
+        vaporPressure: number; // Pa
+        refractive: number;
+        cas: number;
+    };
+    freundlich: {
+        k: number;
+        nth: number;
+    };
+    kinetics: {
+        filmDiffusion: {
+            correlation: boolean;
+            value?: number; // cm/s, ignored when correlation === true
+        };
+        surfaceDiffusion: {
+            correlation: boolean;
+            value?: number; // cm^2/s
+        };
+        poreDiffusion: {
+            correlation: boolean;
+            value?: number; // cm^2/s
+        };
+    };
+}
+
+export class ContaminantStore extends BasicTableWithEditStore<ContaminantParams> {
+    public STORE_NAME = 'ContaminantParams';
+
+    constructor() {
+        super(['database', 'contaminant']);
+    }
+
+    public createNew(): void {
+        this.changesHappen(false);
+        this.activeKey = v4();
+        this.activeRecord = {
+            name: '',
+            description: '',
+            material: {
+                name: '',
+                description: '',
+                molecularWeight: 0, // mg/mmol
+                molarVolume: 0, // mL/gmol
+                boilingPt: 0, // ℃
+                initConcent: 0, // mg/L
+                liquidDensity: 0, // g/mL
+                solubility: 0, // mg/L
+                vaporPressure: 0, // Pa
+                refractive: 0,
+                cas: 0
+            },
+            freundlich: {
+                k: 0,
+                nth: 0
+            },
+            kinetics: {
+                filmDiffusion: {
+                    correlation: false,
+                    value: 0
+                },
+                surfaceDiffusion: {
+                    correlation: false,
+                    value: 0
+                },
+                poreDiffusion: {
+                    correlation: false,
+                    value: 0
+                }
+            }
+        };
+    }
+}
