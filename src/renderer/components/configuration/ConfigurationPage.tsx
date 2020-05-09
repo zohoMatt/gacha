e4e026e6ec8bed3a4858d6054f0f4eae7e833d7c
zohoMatt <mattzuo12@gmail.com>
2020-05-09 17:39:19 -0400

    feat: Remove old elements.

--- src/renderer/components/configuration/ConfigurationPage.tsx
+++ src/renderer/components/configuration/ConfigurationPage.tsx
@@ -2,12 +2,8 @@ import * as React from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 import NavBar from '../nav/NavBar';
-import { WaterProps } from '../experimentPages/profile/water/WaterProps';
-import { SimulationParameter } from '../experimentPages/profile/simParams/SimulationParameter';
 import { ContaminantProps } from '../databasePages/contaminant/contaminant/ContaminantProps';
-import { FixedBedProps } from '../experimentPages/profile/bed/FixedBedProps';
 import { AdsorbentDatabase } from '../databasePages/adsorbent/AdsorbentDatabase';
-import { AdsorptionMechanisms } from '../databasePages/contaminant/adsorption/AdsorptionMechanisms';
 import { ProfileSettings } from '../experimentPages/ProfileSettings';
 
 const styles = require('./ConfigurationPage.module.less');
@@ -27,22 +23,6 @@ const ConfigurationPage: React.FunctionComponent = () => {
                     </Route>
                     <Route path={`${match.path}/exp/profile`}>
                         <ProfileSettings />
-                    </Route>
-
-                    {/* deprecated */}
-                    <Route path={`${match.path}/settings/water/prop`}>
-                        <WaterProps />
-                    </Route>
-                    <Route path={`${match.path}/settings/components/adsorb`}>
-                        <AdsorptionMechanisms />
-                    </Route>
-
-                    <Route path={`${match.path}/settings/sim/psdm`}>
-                        <SimulationParameter />
-                    </Route>
-
-                    <Route path={`${match.path}/settings/bed/prop`}>
-                        <FixedBedProps />
                     </Route>
                 </Switch>
             </div>
