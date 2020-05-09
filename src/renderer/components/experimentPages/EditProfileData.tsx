2a4a3c3138889edca1199812828d2ed67bab73f6
zohoMatt <mattzuo12@gmail.com>
2020-05-09 17:30:21 -0400

    feat: Water tab.

--- src/renderer/components/experimentPages/EditProfileData.tsx
+++ src/renderer/components/experimentPages/EditProfileData.tsx
@@ -3,6 +3,8 @@ import { Form, Tabs } from 'antd';
 
 import { EditProps } from '../common/container/TableWithEditSection';
 import { VALIDATE_MSG_TEMPLATE } from '../../../utils/validator';
+import { EditWaterData } from './profile/water/EditWaterData';
+import { BasicInfoFormFields } from '../common/BasicInfo';
 
 export const EditProfileData: React.FunctionComponent<EditProps> = ({
     form,
@@ -22,8 +24,13 @@ export const EditProfileData: React.FunctionComponent<EditProps> = ({
             wrapperCol={{ span: 8 }}
             onValuesChange={(s, all: any) => onValuesChange(all)}
             initialValues={initValues}>
-            <Tabs defaultActiveKey="water">
-                <TabPane tab="Water" key="water" />
+            <Tabs defaultActiveKey="basic">
+                <TabPane tab="Basic" key="basic">
+                    <BasicInfoFormFields />
+                </TabPane>
+                <TabPane tab="Water" key="water">
+                    <EditWaterData {...initValues.water} />
+                </TabPane>
                 <TabPane tab="Fixed Bed" key="bed" />
                 <TabPane tab="Phys. Chem. Properties" key="physchem" />
                 <TabPane tab="PSDM Simulation" key="psdm" />
--- src/renderer/components/experimentPages/profile/water/EditWaterData.tsx
+++ src/renderer/components/experimentPages/profile/water/EditWaterData.tsx
@@ -1,45 +1,28 @@
 import * as React from 'react';
 import { Divider, Form, Input } from 'antd';
 
-import { BasicInfoFormFields } from '../../../common/BasicInfo';
 import { TextSwitcher } from '../../../common/elements/TextSwticher';
-import { EditProps } from '../../../common/container/TableWithEditSection';
 import { WaterValidator } from '../../../../../mods/validators/water.validator';
-import { VALIDATE_MSG_TEMPLATE } from '../../../../../utils/validator';
 import { calcDensityAndViscosity } from './calculation';
+import { WaterInputParams } from '../../../../store/experiment.store';
 
-export const EditWaterData: React.FunctionComponent<EditProps> = ({
-    form,
-    initValues,
-    onValuesChange
-}) => {
+export const EditWaterData: React.FunctionComponent<WaterInputParams> = initValues => {
     const { temperature } = initValues;
     const vdator = new WaterValidator();
 
     const [density, viscosity] = calcDensityAndViscosity(temperature);
 
     return (
-        <Form
-            size="small"
-            layout="horizontal"
-            ref={form}
-            validateMessages={VALIDATE_MSG_TEMPLATE}
-            hideRequiredMark={true}
-            labelCol={{ span: 6 }}
-            wrapperCol={{ span: 8 }}
-            onValuesChange={(s, all: any) => onValuesChange(all)}
-            initialValues={initValues}>
-            <BasicInfoFormFields />
-            <Divider orientation="left" />
+        <>
             <Form.Item
-                name="pressure"
+                name={['water', 'pressure']}
                 label="Pressure"
                 rules={vdator.getFormValidators('pressure')}
                 normalize={v => (v ? +v : '')}>
                 <Input type="number" addonAfter="atm" />
             </Form.Item>
             <Form.Item
-                name="temperature"
+                name={['water', 'temperature']}
                 label="Temperature"
                 rules={vdator.getFormValidators('temperature')}
                 normalize={v => (v ? +v : '')}>
@@ -47,13 +30,13 @@ export const EditWaterData: React.FunctionComponent<EditProps> = ({
             </Form.Item>
             <Divider orientation="left">Correlations</Divider>
             <Form.Item label="Density">
-                <Form.Item name={['density', 'use']}>
+                <Form.Item name={['water', 'useDensity']}>
                     <TextSwitcher text={temperature < 100 && temperature > 0 ? density : '-'} />
                 </Form.Item>
             </Form.Item>
-            <Form.Item name={['dynamicViscosity', 'use']} label="Viscosity">
+            <Form.Item name={['water', 'useViscosity']} label="Viscosity">
                 <TextSwitcher text={temperature < 100 && temperature > 0 ? viscosity : '-'} />
             </Form.Item>
-        </Form>
+        </>
     );
 };
