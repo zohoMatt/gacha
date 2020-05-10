26505f5f7f89a27d4cdf3c9e66e627a38c141969
zohoMatt <mattzuo12@gmail.com>
2020-05-09 23:48:25 -0400

    feat: Display adsorbent in ViewBedProps.

--- src/renderer/components/experimentPages/profile/bed/ViewBedProps.tsx
+++ src/renderer/components/experimentPages/profile/bed/ViewBedProps.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import { Descriptions } from 'antd';
+import { Link } from 'react-router-dom';
 
 import { BedMaths } from '../../../../../mods/calculation/independent/bed.maths';
 import { Calculation } from '../../../../../mods/calculation/basic';
 import { BedInputParams } from '../../../../store/experiment.store';
 import { WeakTitle } from '../../../common/elements/WeakTitle';
+import { FullRecordType } from '../../../../store/base';
+import { AdsorbentParams } from '../../../../store/adsorbent.store';
+
+// fixme This component use global store directly. Not a good practice.
+// However, the usage of this store is for READONLY without side effects.
+// So no worries but better refactor it.
+import { Store } from '../../../../store/init';
 
 export const ViewBedProps: React.FunctionComponent<BedInputParams> = data => {
-    const { length, diameter, mass, flowrate, ebct } = data;
+    const { adsorbent, length, diameter, mass, flowrate, ebct } = data;
     const areaVal = `${Calculation.format(
         BedMaths.crossSectionalArea(`${diameter}cm`).toNumber('cm^2')
     )} cm²`;
@@ -18,9 +26,22 @@ export const ViewBedProps: React.FunctionComponent<BedInputParams> = data => {
         BedMaths.density(`${mass}g`, `${length}cm`, `${diameter}cm`).toNumber('g/mL')
     )}g/mL`;
 
+    const ads = Store.root.adsorbent.database.props.find(
+        (r: FullRecordType<AdsorbentParams>) => r.key === adsorbent
+    );
+
     return (
         <>
             <Descriptions>
+                <Descriptions.Item label="Adsorbent" span={3}>
+                    {ads ? (
+                        <Link to={`/workspace/database/adsorbent?key=${adsorbent}`}>
+                            {ads.name}
+                        </Link>
+                    ) : (
+                        'N/A'
+                    )}
+                </Descriptions.Item>
                 <Descriptions.Item label="Bed Length">{`${length} cm`}</Descriptions.Item>
                 <Descriptions.Item label="Bed Diameter">{`${diameter} cm`}</Descriptions.Item>
                 <Descriptions.Item label="Mass">{`${mass} g`}</Descriptions.Item>
--- src/renderer/components/nav/NavBar.tsx
+++ src/renderer/components/nav/NavBar.tsx
@@ -7,54 +7,69 @@ import { ExperimentOutlined, SettingOutlined } from '@ant-design/icons/lib';
 
 const styles = require('./NavBar.module.less');
 
-const { SubMenu, ItemGroup, Item } = Menu;
-// Hack for tslint
-const Group = ItemGroup as any;
+const { SubMenu, Item } = Menu;
 
 const NavBar: React.FunctionComponent = () => {
     const match = useRouteMatch();
+    const NAV_KEYS = {
+        Database: 'database',
+        Experiment: 'experiment',
+        Plot: 'plot',
+        System: 'system'
+    };
+
+    const ITEM_KEYS = {
+        Contaminant: 'contaminant',
+        Adsorbent: 'adsorbent',
+        Profile: 'profile',
+        Result: 'result',
+        Comparison: 'comparison',
+        History: 'history',
+        Prefer: 'prefer'
+    };
+
     return (
         <div className={styles.navbar}>
-            <Menu style={{ width: '100%' }} mode="inline">
+            <Menu style={{ width: '100%' }} defaultOpenKeys={Object.values(NAV_KEYS)} mode="inline">
                 <SubMenu
-                    key="database"
+                    key={NAV_KEYS.Database}
                     title={
                         <span>
                             <DatabaseOutlined />
                             <span>Database</span>
                         </span>
                     }>
-                    <Item key="contaminants">
+                    <Item key={ITEM_KEYS.Contaminant}>
                         <Link to={`${match.url}/database/contaminants`}>Contaminants</Link>
                     </Item>
-                    <Item key="adsorbent">
+                    <Item key={ITEM_KEYS.Adsorbent}>
                         <Link to={`${match.url}/database/adsorbent`}>Adsorbent</Link>
                     </Item>
                 </SubMenu>
                 <SubMenu
-                    key="experiment"
+                    key={NAV_KEYS.Experiment}
                     title={
                         <span>
                             <ExperimentOutlined />
                             <span>Experiment</span>
                         </span>
                     }>
-                    <Item key="profile">
+                    <Item key={ITEM_KEYS.Profile}>
                         <Link to={`${match.url}/exp/profile`}>Profile</Link>
                     </Item>
                 </SubMenu>
                 <SubMenu
-                    key="plot"
+                    key={NAV_KEYS.Plot}
                     title={
                         <span>
                             <AreaChartOutlined />
                             <span>Plot</span>
                         </span>
                     }>
-                    <Item key="results">
+                    <Item key={ITEM_KEYS.Result}>
                         <Link to={`${match.url}/plot/res`}>Results</Link>
                     </Item>
-                    <Item key="comparison">
+                    <Item key={ITEM_KEYS.Comparison}>
                         <Link to={`${match.url}/plot/compare`}>Comparison</Link>
                     </Item>
                 </SubMenu>
@@ -69,7 +84,7 @@ const NavBar: React.FunctionComponent = () => {
                     <Item key="history">
                         <Link to={`${match.url}/sys/history`}>History</Link>
                     </Item>
-                    <Item key="prefer">
+                    <Item key={ITEM_KEYS.Prefer}>
                         <Link to={`${match.url}/sys/pref`}>Preference</Link>
                     </Item>
                 </SubMenu>
