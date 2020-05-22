9fd71d3a5a6871244deba2f720f457b7d9fec950
zohoMatt <mattzuo12@gmail.com>
2020-05-22 16:48:54 -0400

    project: Fix the sourcemap -- only works after 2nd compiling?

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -15,7 +15,7 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js', '.json']
     },
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     plugins: [
     ]
 };
