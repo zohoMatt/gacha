2f6a5bebd8b1714f8f38df399c02801bffb96e93
zohoMatt <mattzuo12@gmail.com>
2020-06-02 11:41:55 -0400

    feat: Title of Comparison page.

--- src/renderer/components/plotPages/comparison/GraphComparison.tsx
+++ src/renderer/components/plotPages/comparison/GraphComparison.tsx
@@ -3,18 +3,21 @@ import { Tabs } from 'antd';
 
 import { MultiLineScatterGraph } from './concentrationByTime/MultiLineScatterGraph';
 import { ContourGraph } from './penetrationProcess/ContourGraph';
+import { Workspace } from '../../common/container/Workspace';
 
 export const GraphComparison: React.FC = () => {
     const { TabPane } = Tabs;
 
     return (
-        <Tabs defaultActiveKey="scatter">
-            <TabPane tab="Scatter Plot" key="scatter">
-                <MultiLineScatterGraph />
-            </TabPane>
-            <TabPane tab="Contour Graph" key="contour">
-                <ContourGraph />
-            </TabPane>
-        </Tabs>
+        <Workspace title="Comparison of Results">
+            <Tabs defaultActiveKey="scatter">
+                <TabPane tab="Scatter Plot" key="scatter">
+                    <MultiLineScatterGraph />
+                </TabPane>
+                <TabPane tab="Contour Graph" key="contour">
+                    <ContourGraph />
+                </TabPane>
+            </Tabs>
+        </Workspace>
     );
 };
