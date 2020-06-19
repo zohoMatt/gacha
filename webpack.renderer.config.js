f9945d3c7f4c776479717ebad9c8f8d5e7a9143d
zohoMatt <mattzuo12@gmail.com>
2020-06-19 15:59:16 -0400

    proj: Update html-webpack-plugin.

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -106,7 +106,9 @@ module.exports = merge.smart(baseConfig, {
             reportFiles: ['src/renderer/**/*']
         }),
         new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            base: '/'
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
         })
